b622030e547e63c00ee0394413a3797636b4d35b
Sumit Sarkar <sumitsarkarbd@gmail.com>
2025-09-03 01:28:31 +0600

    HashMap and Set

--- 03StringsAndHashing/duplicate_detector.py
+++ 03StringsAndHashing/duplicate_detector.py
@@ -24,7 +24,7 @@ class DuplicateDetector:
                 duplicates.add(num)
             else:
                 seen.add(num)
-        return list(duplicates)
+        return  list(duplicates)
 
 
 # Main program
